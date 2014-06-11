82cf8a898687e457d17179c4a9e64cd125a3ce2c
Arjun Comar <nrujac@gmail.com>
2014-06-11 14:31:49 -0400

    Working connection to port 80 for the hangman demo.

--- cookbooks/hangman/recipes/default.rb
+++ cookbooks/hangman/recipes/default.rb
@@ -9,6 +9,7 @@
 
 include_recipe 'apache2::mod_proxy'
 include_recipe 'apache2::mod_proxy_ajp'
+include_recipe 'apache2::mod_proxy_http'
 
 dbi = Chef::DataBagItem.load("chef_demo", "nexus")
 
@@ -26,14 +27,7 @@ package 'hangman' do
     action :install
 end
 
-template "/etc/httpd/conf.d/proxy_ajp.conf" do
-    source "proxy_ajp.conf.erb"
+template '/etc/httpd/conf.d/proxy_ajb.conf' do
+    source 'proxy_ajp.conf.erb'
 end
 
-template "/usr/share/tomcat6/conf/server.xml" do
-    source "server.xml.erb"
-end
-
-server 'apache2' do
-    action :restart
-end
